43efdc1766f579c2a5fe8423b4a02afb14c822bb
Fearghus Tyler <Fearghus333@gmail.com>
2019-11-21 20:48:36 -0800

    mee

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
-$.getJSON("/articles", function (data) {
-  for (var i = 0; i < data.length; ++i) {
+$.getJSON("/articles", (data) => {
+  for (var i = 0; i < data.length; i++) {
     $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
   }
 });
